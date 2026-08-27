a588a624a2bfb876f96acaf4953a45c816af7cdb
Major Arcana <MummyPapa@Rashis-MacBook-Air.local>
2026-08-27 10:20:36 +0530

    Complete Task 0 solutions

--- Python_Coding/2026A7PS0458P SiG Project for recruitment/q1.py
+++ Python_Coding/2026A7PS0458P SiG Project for recruitment/q1.py
@@ -9,4 +9,30 @@ if numbers:
     even_count = [0]
     odd_count = [0]
 
-    
+for item in numbers:
+    n = item
+
+    if n > x:
+        x = n
+
+    if n < y:
+        y = n
+
+    total_sum[0] += n
+
+    if n % 2 == 0:
+        even_count[0] += 1
+    else:
+        odd_count[0] += 1
+
+    mirror_list = []
+    for i in range(len(numbers)):
+        mirror_list.append(numbers[-1 -i])
+
+print("The largest number is:", x)
+print("The smallest number is:", y)
+print("The sum of all numbers is:", total_sum[0])
+print("The count of even numbers is:", even_count[0])
+print("The count of odd numbers is:", odd_count[0])
+print("The mirror list is:", mirror_list)
+
