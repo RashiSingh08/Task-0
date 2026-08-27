3d8b74bcd5775d943c5336c72df5c6e98cd33a78
Major Arcana <MummyPapa@Rashis-MacBook-Air.local>
2026-08-27 11:31:11 +0530

    This was actually fun

--- Python_Coding/2026A7PS0458P SiG Project for recruitment/q3.py
+++ Python_Coding/2026A7PS0458P SiG Project for recruitment/q3.py
@@ -2,11 +2,15 @@
 
 def is_prime(n):
     if n <= 1:
+        pp = ("Number is less than 1!!")
         return False
+        
     for i in range(2, int(n**0.5) +1):
         if n%i == 0:
+            pp = ("Number is composite")
             break
     else:
+        pp = ("Number is prime")
         return True
     return False
 
@@ -15,9 +19,19 @@ def is_prime(n):
 
 def all_primes(N):
     primes = []
-    for i in range (2, N):
-        if is_prime(n):
-            all_primes.append(i)
+    for i in range (2, N+1):
+        if is_prime(i):
+            primes.append(i)
+    return primes
 
 n = int(input("Enter a number for checking if it's prime or not: "))
+if is_prime(n):
+    print(f"{n} is prime")
+else:
+    if n<=1:
+        print(f"{n} is less than 1")
+    else: 
+        print(f"{n} is composite")
+
 N = int(input("Enter a number for generating all primes till that number: "))
+print(*all_primes(N))
